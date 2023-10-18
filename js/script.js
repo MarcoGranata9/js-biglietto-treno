14bdb7802c77eaa90114ce666aacbf78d2788215
MarcoGranata9 <granatam9900@gmail.com>
2023-10-18 16:29:28 +0200

    add: data prompt

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Dati
+const distance = parseInt(prompt("Quanti km devi percorrere?"))
+
+const age = parseInt(prompt("Quanti anni hai?"))
+
+const ticketPrice = 0.21
+console.log(distance, age, ticketPrice)
